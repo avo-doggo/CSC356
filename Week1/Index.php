4224baca142ec15c138c31519830c46686b3f3ce
Jameson Brown <144629151+avo-doggo@users.noreply.github.com>
2025-11-09 22:44:34 -0600

    Update Index.php

--- Week1/Index.php
+++ Week1/Index.php
@@ -7,17 +7,19 @@
      <link rel="stylesheet" href="PilotApp.css">
 </head>
 <body>
-
+<!--navigation menu-->
     <nav>
         <ul>
             <li><a href="Index.php">Home</a></li>
             <li><a href="PilotApp.php">Pilot Application</a></li>
         </ul>
     </nav>
-
+<!--short company description-->
     <h1> Opportunity Program </h1>
+    <!--Named after a mars rover that stopped working in 2019?-->
     <p> Here at Opportunity, we are working towards a brighter tomorrow.<br>
      Starting with trips to Mars, we are hoping to show people the beauty of space!<br>
      As of now, we are in need of Opportunity Pilots! If you or someone you know is interested, click the button below!<br>
      <a href="PilotApp.php"><button> Application</button></a>
+
 </html>
