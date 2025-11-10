60823f719cf5821a6792c773a50da53cc3577a66
Jameson Brown <144629151+avo-doggo@users.noreply.github.com>
2025-11-09 22:36:57 -0600

    Add files via upload

---
+++ Week1/Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opportunity Homepage</title>
+     <link rel="stylesheet" href="PilotApp.css">
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="Index.php">Home</a></li>
+            <li><a href="PilotApp.php">Pilot Application</a></li>
+        </ul>
+    </nav>
+
+    <h1> Opportunity Program </h1>
+    <p> Here at Opportunity, we are working towards a brighter tomorrow.<br>
+     Starting with trips to Mars, we are hoping to show people the beauty of space!<br>
+     As of now, we are in need of Opportunity Pilots! If you or someone you know is interested, click the button below!<br>
+     <a href="PilotApp.php"><button> Application</button></a>
+</html>
---
+++ Week1/PilotApp.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mars Pilot Application</title>
+    <script src="AppValidator.js"></script>
+    <link rel="stylesheet" href="PilotApp.css">
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="Index.php">Home</a></li>
+            <li><a href="PilotApp.php">Pilot Application</a></li>
+        </ul>
+    </nav>
+
+    <h1> Mars Pilot Application </h1>
+
+    <form name="appForm" onsubmit="return validateForm();" action="#" method="post">
+        <div class="row">
+            <label>Full Name</label>
+            <input type="text" name="fullName">
+            <div class="error" id="fullNameErr"></div>
+        </div>
+
+        <div class="row">
+            <label>Age</label>
+            <input type="text" name="age">
+            <div class="error" id="ageErr"></div>
+        </div>
+
+        <div class="row">
+            <label>ID Number</label>
+            <input type="text" name="idNum">
+            <div class="error" id="idNumErr"></div>
+        </div>
+
+        <div class="row">
+            <label>Weight</label>
+            <input type="text" name="weight">
+            <div class="error" id="weightErr"></div>
+        </div>
+
+        <div class="row">
+            <label>Are You Afraid of Heights?</label>
+            <div class="form-inline">
+                <label><input type="radio" name="fear" value="yes"> Yes</label>
+                <label><input type="radio" name="fear" value="no"> No</label>
+            </div>
+            <div class="error" id="fearErr"></div>
+        </div>
+            <input type="submit" value="Submit">
+        <div class="row">
+
+        </div>
+    </form>
+</body>
+</html>
